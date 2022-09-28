fc4fc3518567f1678be5e22a7593018a2b5e77c3
Marco roman <mbr89@txstate.edu>
2022-09-28 16:42:54 -0500

    fixed checkinput function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,29 +4,22 @@ import { useState } from 'react';
 
 function App() {
   document.title = 'crazy eights'
-  const [data,setData] = useState(1);
+  const [data,setData] = useState(0);
 
   const checkInput = (event) =>{
-    console.log("vener " + event.target.value)
     const warn = document.querySelector("#warn");
   
-    let val = parseInt(event.target.value);
-    console.log("before" + data)
+    const val= parseInt(event.target.value);
     if(!isNaN(event.target.value)){
-      console.log("after" + data)
       console.log(typeof(event.target.value))
       console.log(event.target.value)
       if(val< 1 || val > 5){
         warn.innerText = "please make sure to have 1-5 players"
-        
-        setTimeout(function(){
-          warn.innerText = '';
-          }, 3000);
-          setData(1)
+        setData(0)
+      }else{
+        setData(val)
+        warn.innerText = "";
       }
-
-    }else{
-      warn.innerText = "";
     }
   }
 
@@ -34,18 +27,19 @@ function App() {
     <div>
       <h1 style={{textAlign:'center'}}>Welcome to crazy eights</h1>
       <div style={{display:'grid',justifyContent:'center', gridTemplateColumns:'auto 100px' }}>
-        <label style={{textAlign:'center',padding:'5px'}}>How many players? {data} </label>
+        <label style={{textAlign:'center',padding:'5px'}}>How many players? </label>
         <input id="playerCount" onChange={(e)=>{checkInput(e)}} type="number" min="1" max="5" required-pattern="/d{1}" />
       </div>
-      <p id="warn" style={{textAlign:'center'}}></p>
+      <p id="warn" style={{textAlign:'center',color:'red'}}></p>
+      <p id="totalPlayers" style={{textAlign:'center'}} >{data>0?`${data} players`:''}</p>
       
       <div style={{display:'flex',justifyContent:'center'}}>
       <footer style={{position:'fixed',bottom:'0'}}>
-          <div style={{display:'grid',justifyContent:'center',gridTemplateColumns:'repeat(4,auto)', gridGap:'5px'}}>
-            <p>Login</p>
-            <p>About us</p>
-            <p>Donate</p>
-            <p>Tutorial</p>
+          <div style={{display:'grid',justifyContent:'center',gridTemplateColumns:'repeat(4,auto)', gap:'5px',padding:'5px'}}>
+            <a href="#">Login</a>
+            <a href="#">About us</a>
+            <a href="#">Donate</a>
+            <a href="#">Tutorial</a>
           </div>
         
       </footer>
