6b4bc7b0c6eaa6226bbb25c1b1f61ee9e308971a
Tristan Stuart <pme28@txstate.edu>
2022-10-04 19:04:43 -0500

    Created aboutUs page css and html and added entry to flask app

--- app.py
+++ app.py
@@ -18,6 +18,10 @@ def signup():
 def home():
 	return render_template('index.html')
 
+@app.route('/about_us')
+def about_us():
+	return render_template('about_us.html')
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
