c1742012fb73f5dc21ce5b1f0947e35214a94214
Marco roman <mbr89@txstate.edu>
2022-10-19 11:50:38 -0500

    added comments

--- frontend/client/src/components/Card.js
+++ frontend/client/src/components/Card.js
@@ -1,5 +1,7 @@
 import {useState} from 'react'
 //assuming card svg's are stored in public/cards
+//takes in a string named type that declares the type of Card
+//Ex AD - Ace of Diamonds etc.
 const Card = (props) =>{
     //gives errors, too many renders?
     // const [rank,setRank] = useState("");
