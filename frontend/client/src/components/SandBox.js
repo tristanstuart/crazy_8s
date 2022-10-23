958466ee1dfd2ba269340425659c74b88eeac3ab
Christian Davis <cld113@txstate.edu>
2022-10-23 15:41:52 -0500

    updated sandbox

--- frontend/client/src/components/SandBox.js
+++ frontend/client/src/components/SandBox.js
@@ -6,19 +6,19 @@ export default function SandBox(){
     return(
         <div className="container mx-auto shadow-md bg-green-300 md:max-w-2xl">
             <div className="flex flex-grow">
-                    <Card rank={'1'} suits={'hearts'} />
-                    <Card rank={'2'} suits={'hearts'}  />
-                    <Card rank={'3'} suits={'hearts'} />
-                    <Card rank={'4'} suits={'hearts'} />
-                    <Card rank={'5'} suits={'hearts'} />
-                    <Card rank={'6'} suits={'hearts'} />
-                    <Card rank={'7'} suits={'hearts'} />
-                    <Card rank={'8'} suits={'hearts'} />
-                    <Card rank={'9'} suits={'hearts'} />
-                    <Card rank={'10'} suits={'hearts'} />
-                    <Card rank={'11'} suits={'hearts'} />
-                    <Card rank={'12'} suits={'hearts'} />
-                    <Card rank={'13'} suits={'hearts'} />
+                    <Card rank={'9'} suit={'Diamonds'} />
+                    {/* <Card rank={'2'} suit={'Hearts'}  />
+                    <Card rank={'3'} suit={'Hearts'} />
+                    <Card rank={'4'} suit={'Hearts'} />
+                    <Card rank={'5'} suit={'Hearts'} />
+                    <Card rank={'6'} suit={'Hearts'} />
+                    <Card rank={'7'} suit={'Hearts'} />
+                    <Card rank={'8'} suit={'Hearts'} />
+                    <Card rank={'9'} suit={'Hearts'} />
+                    <Card rank={'10'} suit={'Hearts'} />
+                    <Card rank={'11'} suit={'Hearts'} />
+                    <Card rank={'12'} suit={'Hearts'} />
+                    <Card rank={'13'} suit={'Hearts'} /> */}
 
             </div>
             container
