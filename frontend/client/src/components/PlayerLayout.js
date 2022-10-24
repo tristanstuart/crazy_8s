0b812ec99e97fcbc3ff0a8dd3fb9b262041618a5
Christian Davis <cld113@txstate.edu>
2022-10-24 17:05:37 -0500

    created player layout

---
+++ frontend/client/src/components/PlayerLayout.js
@@ -0,0 +1,44 @@
+
+function TableLayout(props){
+    //referenced https://css-tricks.com/text-blocks-over-image/ for displaying text over the card image
+    const avatar = <img alt='avatar icon' src='../../avatar.svg' className='rounded-lg w-20 mb-4' />
+    const deckImg = <img alt="1B" src="../../cards/1B.svg" className="w-15 h-20"/>
+
+    
+    let player_layout = 'flex pl-20 pr-20 items-center justify-center'
+    let playerIcon = []
+    props.opponents.forEach(person => {
+        playerIcon.push(
+            <div 
+                key={person.name} 
+                className={player_layout}
+            >
+                <div 
+                    className='text-center'
+                >
+                    {avatar}
+                    <h5>
+                        {person['name']}
+                    </h5>
+                    <div 
+                        className=' relative flex justify-center p-3' >
+                        {deckImg}
+
+                        <div className='text-8xl absolute'>
+                            {person['count']}
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        )
+    })
+
+    return (
+        <div className="inline-flex">
+            {playerIcon}
+        </div>
+    )
+}
+
+export default TableLayout;
