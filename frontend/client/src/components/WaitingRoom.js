1a5a0fe0796f34b3554c3d0edcb7d23ea32228ae
Marco roman <mbr89@txstate.edu>
2022-11-21 13:03:04 -0600

    finished leaderboard component

--- frontend/client/src/components/WaitingRoom.js
+++ frontend/client/src/components/WaitingRoom.js
@@ -41,11 +41,6 @@ function WaitingRoom({ socket }) {
             setPlayers(DATA.playerList)
         })
 
-        //work on this
-        socket.on("winner",e=>{
-            console.log(e)
-        })
-
         socket.on("override",data=>{
             DATA.turn = data["nextTurn"]
             sessionStorage.setItem("data",JSON.stringify(DATA))
