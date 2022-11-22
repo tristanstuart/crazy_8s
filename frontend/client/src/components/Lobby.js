71d0af6b77ab951b015d39886d6c6afb0e0719a4
Christian Davis <christiandavis@cd-mac.local>
2022-11-21 18:22:28 -0600

    adjusted naming convention so warnings would not popup in console

--- frontend/client/src/components/Lobby.js
+++ frontend/client/src/components/Lobby.js
@@ -10,33 +10,33 @@ function Lobby({socket, players, isAdmin, ROOM, ID, DATA}){
     const icon_color = ['#d6d3d1', '#a16207', "#d8b4fe", "#c7d2fe", "#fafaf9",]
     return(
         <div>
-            <div class="min-h-screen flex-1 bg-gray-200 p-4 flex justify-center items-center">
-                <div class="bg-white w-full md:max-w-4xl rounded-lg shadow">
-                <div class="h-12 flex justify-between items-center border-b border-gray-200 m-4">
+            <div className="min-h-screen flex-1 bg-gray-200 p-4 flex justify-center items-center">
+                <div className="bg-white w-full md:max-w-4xl rounded-lg shadow">
+                <div className="h-12 flex justify-between items-center border-b border-gray-200 m-4">
                 <div >
-                    <div class="text-xl font-bold text-gray-700">
+                    <div className="text-xl font-bold text-gray-700">
                         Game Lobby: {ROOM}
                     </div>
-                    <div class="text-sm font-base text-gray-500 block">
+                    <div className="text-sm font-base text-gray-500 block">
                         <Loading/>
                     </div>
                 </div>
                 <div>
-                    <div class="flex items-center justify-center w-full  shadow-md rounded-full">
+                    <div className="flex items-center justify-center w-full  shadow-md rounded-full">
                     <label
                         htmlFor="toogleA"
-                        class="flex items-center cursor-pointer"
+                        className="flex items-center cursor-pointer"
                     >
-                        <div class="flex items-center">
-                            <input id="toogleA" type="checkbox" class="hidden"/>
+                        <div className="flex items-center">
+                            <input id="toogleA" type="checkbox" className="hidden"/>
                         <div
-                            class="toggle__line w-20 h-10 bg-gray-300 rounded-full shadow-inner"
+                            className="toggle__line w-20 h-10 bg-gray-300 rounded-full shadow-inner"
                         >
                         </div>
                         <div
-                            class="toggle__dot bg-red-400 absolute w-10 h-10 bg-white rounded-full shadow flex items-center justify-center"
+                            className="toggle__dot bg-red-400 absolute w-10 h-10 bg-white rounded-full shadow flex items-center justify-center"
                         >
-                            <svg class="text-white w-6 h-6 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <svg className="text-white w-6 h-6 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z" />
                             </svg>
                         </div>
@@ -45,21 +45,21 @@ function Lobby({socket, players, isAdmin, ROOM, ID, DATA}){
                     </div>
                 </div>
                 </div>
-                <div class="px-6">
+                <div className="px-6">
                     <ul>
                         {players.map(data =>
                             (
-                                <div class="flex justify-between items-center h-16 p-4 my-6  rounded-lg border border-gray-100 shadow-md">
-                                    <div class="flex items-center">
-                                    {/* <img class="" alt="Avatar" /> */}
+                                <div className="flex justify-between items-center h-16 p-4 my-6  rounded-lg border border-gray-100 shadow-md">
+                                    <div className="flex items-center">
+                                    {/* <img className="" alt="Avatar" /> */}
                                     <FontAwesomeIcon size="2x" icon={icons[playerCount]} color={icon_color[playerCount++]} />
-                                        <div class="ml-2">
-                                            <div class="text-sm font-semibold text-gray-600">{data}</div>
+                                        <div className="ml-2">
+                                            <div className="text-sm font-semibold text-gray-600">{data}</div>
                                         </div>
                                     </div>
                                     <div>
-                                        <button class="bg-green-400 hover:bg-red-500 p-2 rounded-full shadow-md flex justify-center items-center">
-                                            {/* <svg class="text-white toggle__lock w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <button className="bg-green-400 hover:bg-red-500 p-2 rounded-full shadow-md flex justify-center items-center">
+                                            {/* <svg className="text-white toggle__lock w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                                             </svg> */}
                                             <FontAwesomeIcon icon={faCheck} />
@@ -69,21 +69,21 @@ function Lobby({socket, players, isAdmin, ROOM, ID, DATA}){
                             ))}
                     </ul>
 
-                <div class="flex bg-gray-200 justify-center items-center h-16 p-4 my-6  rounded-lg  shadow-inner">
-                    <div class="flex items-center border border-gray-400 p-2 border-dashed rounded cursor-pointer">
+                <div className="flex bg-gray-200 justify-center items-center h-16 p-4 my-6  rounded-lg  shadow-inner">
+                    <div className="flex items-center border border-gray-400 p-2 border-dashed rounded cursor-pointer">
                     <div>
-                    <svg class="text-gray-500 w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg className="text-gray-500 w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                     </svg>
                     </div>
-                    <div class="ml-1 text-gray-500 font-medium"> Invite a friend</div>
+                    <div className="ml-1 text-gray-500 font-medium"> Invite a friend</div>
                     </div>
                 </div>
                 </div>
-                    <div class="p-6 ">
+                    <div className="p-6 ">
                         {isAdmin && (
                             <button 
-                                class="p-4 bg-green-400 hover:bg-green-500 w-full rounded-lg shadow text-xl font-medium uppercase text-white"
+                                className="p-4 bg-green-400 hover:bg-green-500 w-full rounded-lg shadow text-xl font-medium uppercase text-white"
                                 onClick={() => {
 
                                     socket.emit("start_game", {
@@ -94,7 +94,7 @@ function Lobby({socket, players, isAdmin, ROOM, ID, DATA}){
                             Start the Game
                             </button>)}
                     </div>
-                    <div class="p-6">
+                    <div className="p-6">
                         <LeaveGame
                             socket={socket} 
                             room={ROOM} 
